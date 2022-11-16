9e9d261ad6561698329c19f52f602b576786f8a8
Philip Lin <phil@Philips-MacBook-Air.local>
2022-11-15 17:42:25 -0800

    Changed origin.

--- app.js
+++ app.js
@@ -14,9 +14,7 @@ const mongoose = require("mongoose");
 const app = express();
 
 const cors = require('cors');
-app.use(cors({
-    origin: "http://localhost:3000"
-}));
+app.use(cors());
 
 
 //Body parser for requests
